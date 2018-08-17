07d7d9622ea2e52d3ce483bc79ff259f6d932f94
Lê Thanh Tùng <tunglt@iotech.vn>
2018-08-17 17:38:01 +0700

    finished interface mangebookviewcontroller

---
+++ JVB-BookLibrary/Controller/ManageBookViewController/Cells/ManagerBookTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ManagerBookTableViewCell.swift
+//  JVB-BookLibrary
+//
+//  Created by Admin on 8/17/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ManagerBookTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ JVB-BookLibrary/View/AddBookView/AddBookView.swift
@@ -0,0 +1,21 @@
+//
+//  AddBookView.swift
+//  JVB-BookLibrary
+//
+//  Created by Admin on 8/17/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import UIKit
+
+class AddBookView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
